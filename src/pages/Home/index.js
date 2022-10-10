3dda133ac1ec64cd37a02e32390b1887ce7ae77c
unknown <tgantara@gmail.com>
2022-10-10 16:04:21 +0700

    update card

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -4,12 +4,13 @@ import Navigation from '../../assets/icon/navigation.svg'
 import MobileData from '../../assets/icon/mobiledata.svg'
 import Slider from 'react-slick'
 import ReactStars from "react-rating-stars-component";
-
+import TicketIcon from "../../assets/icon/ticket.svg"
 function index() {
   const setting = {
     dots: false,
     infinite: false,
     speed: 500,
+    autoplay:true,
     slidesToShow: 5,
     slidesToScroll: 1,
     initialSlide: 0,
@@ -210,6 +211,13 @@ function index() {
               <p className="description-content-top-destination">Lorem ipsum dolor sit.</p>
             </div>
           </div>
+          <div className="container-content-top-destination">
+            <img src="https://images.unsplash.com/photo-1536152470836-b943b246224c?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=738&q=80" alt="" srcset="" className='img-content-top-destination' />
+            <div className="body-content-top-destination">
+              <p className="title-content-top-destination">Lorem, ipsum.</p>
+              <p className="description-content-top-destination">Lorem ipsum dolor sit.</p>
+            </div>
+          </div>
         </div>
         <div className="container-link-top-destination">
           <div className="body-link-top-destination">
@@ -249,21 +257,283 @@ function index() {
                 />
                 <p className='text-review-content-card-tours'>24 Reviews</p>
               </div>
-            </div>
-            <p className="title-card-tours">
-              Lorem ipsum dolor sit amet consectetur.
-            </p>
-            <div className="description-card-tours">
+              <p className="title-card-tours">
+                Lorem ipsum dolor sit amet consectetur.
+              </p>
+              <div className="description-card-tours">
                 <p>Lorem ipsum dolor sit.</p>
                 <p>Lorem, ipsum.</p>
+              </div>
+              <div className="description-card-tours">
+                <p>Lorem, ipsum.</p>
+                <p>Lorem, ipsum.</p>
+              </div>
+              <div className="description-card-tours">
+                <p>Lorem, ipsum.</p>
+                <p>Lorem, ipsum.</p>
+              </div>
+              <div className="available-ticket-card-tours">
+                <p>Available Today</p>
+                <div className='ticket-available-ticket-card-tours'>
+                  <img src={TicketIcon} className="icon-available-ticket-card-tours" alt="" srcset="" />
+                  <p>300 Sold</p>
+                </div>
+              </div>
             </div>
-            <div className="description-card-tours">
-                <p>Lorem, ipsum.</p>
-                <p>Lorem, ipsum.</p>
+          </div>
+          <div className="container-card-tours">
+            <div className="body-card-tours">
+              <Slider {...settings}>
+                <img src="https://images.unsplash.com/photo-1470770841072-f978cf4d019e?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1470&q=80" alt="" srcset="" className='img-card-tour' />
+                <img src="https://images.unsplash.com/photo-1501785888041-af3ef285b470?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1470&q=80" alt="" srcset="" className='img-card-tour' />
+                <img src="https://images.unsplash.com/photo-1470770841072-f978cf4d019e?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1470&q=80" alt="" srcset="" className='img-card-tour' />
+              </Slider>
+              <div className="content-top-card">
+                <button className='button-top-card'>lorem</button>
+                <span className="lnr lnr-heart" style={{ color: 'white', fontSize: 20, fontWeight: 'bold' }}></span>
+              </div>
+              <div className="content-bottom-card">
+                <span className="lnr lnr-map-marker text-content-bottom-card"></span>
+                <p>Bandung</p>
+              </div>
             </div>
-            <div className="description-card-tours">
+            <div className="content-card-tours">
+              <div className="review-content-card-tours">
+                <ReactStars
+                  count={5}
+                  onChange={null}
+                  size={24}
+                  value={4}
+                  activeColor="#FF4248"
+                />
+                <p className='text-review-content-card-tours'>24 Reviews</p>
+              </div>
+              <p className="title-card-tours">
+                Lorem ipsum dolor sit amet consectetur.
+              </p>
+              <div className="description-card-tours">
+                <p>Lorem ipsum dolor sit.</p>
+                <p>Lorem, ipsum.</p>
+              </div>
+              <div className="description-card-tours">
                 <p>Lorem, ipsum.</p>
                 <p>Lorem, ipsum.</p>
+              </div>
+              <div className="description-card-tours">
+                <p>Lorem, ipsum.</p>
+                <p>Lorem, ipsum.</p>
+              </div>
+              <div className="available-ticket-card-tours">
+                <p>Available Today</p>
+                <div className='ticket-available-ticket-card-tours'>
+                  <img src={TicketIcon} className="icon-available-ticket-card-tours" alt="" srcset="" />
+                  <p>300 Sold</p>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div className="container-card-tours">
+            <div className="body-card-tours">
+              <Slider {...settings}>
+                <img src="https://images.unsplash.com/photo-1470770841072-f978cf4d019e?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1470&q=80" alt="" srcset="" className='img-card-tour' />
+                <img src="https://images.unsplash.com/photo-1501785888041-af3ef285b470?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1470&q=80" alt="" srcset="" className='img-card-tour' />
+                <img src="https://images.unsplash.com/photo-1470770841072-f978cf4d019e?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1470&q=80" alt="" srcset="" className='img-card-tour' />
+              </Slider>
+              <div className="content-top-card">
+                <button className='button-top-card'>lorem</button>
+                <span className="lnr lnr-heart" style={{ color: 'white', fontSize: 20, fontWeight: 'bold' }}></span>
+              </div>
+              <div className="content-bottom-card">
+                <span className="lnr lnr-map-marker text-content-bottom-card"></span>
+                <p>Bandung</p>
+              </div>
+            </div>
+            <div className="content-card-tours">
+              <div className="review-content-card-tours">
+                <ReactStars
+                  count={5}
+                  onChange={null}
+                  size={24}
+                  value={4}
+                  activeColor="#FF4248"
+                />
+                <p className='text-review-content-card-tours'>24 Reviews</p>
+              </div>
+              <p className="title-card-tours">
+                Lorem ipsum dolor sit amet consectetur.
+              </p>
+              <div className="description-card-tours">
+                <p>Lorem ipsum dolor sit.</p>
+                <p>Lorem, ipsum.</p>
+              </div>
+              <div className="description-card-tours">
+                <p>Lorem, ipsum.</p>
+                <p>Lorem, ipsum.</p>
+              </div>
+              <div className="description-card-tours">
+                <p>Lorem, ipsum.</p>
+                <p>Lorem, ipsum.</p>
+              </div>
+              <div className="available-ticket-card-tours">
+                <p>Available Today</p>
+                <div className='ticket-available-ticket-card-tours'>
+                  <img src={TicketIcon} className="icon-available-ticket-card-tours" alt="" srcset="" />
+                  <p>300 Sold</p>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div className="container-card-tours">
+            <div className="body-card-tours">
+              <Slider {...settings}>
+                <img src="https://images.unsplash.com/photo-1470770841072-f978cf4d019e?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1470&q=80" alt="" srcset="" className='img-card-tour' />
+                <img src="https://images.unsplash.com/photo-1501785888041-af3ef285b470?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1470&q=80" alt="" srcset="" className='img-card-tour' />
+                <img src="https://images.unsplash.com/photo-1470770841072-f978cf4d019e?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1470&q=80" alt="" srcset="" className='img-card-tour' />
+              </Slider>
+              <div className="content-top-card">
+                <button className='button-top-card'>lorem</button>
+                <span className="lnr lnr-heart" style={{ color: 'white', fontSize: 20, fontWeight: 'bold' }}></span>
+              </div>
+              <div className="content-bottom-card">
+                <span className="lnr lnr-map-marker text-content-bottom-card"></span>
+                <p>Bandung</p>
+              </div>
+            </div>
+            <div className="content-card-tours">
+              <div className="review-content-card-tours">
+                <ReactStars
+                  count={5}
+                  onChange={null}
+                  size={24}
+                  value={4}
+                  activeColor="#FF4248"
+                />
+                <p className='text-review-content-card-tours'>24 Reviews</p>
+              </div>
+              <p className="title-card-tours">
+                Lorem ipsum dolor sit amet consectetur.
+              </p>
+              <div className="description-card-tours">
+                <p>Lorem ipsum dolor sit.</p>
+                <p>Lorem, ipsum.</p>
+              </div>
+              <div className="description-card-tours">
+                <p>Lorem, ipsum.</p>
+                <p>Lorem, ipsum.</p>
+              </div>
+              <div className="description-card-tours">
+                <p>Lorem, ipsum.</p>
+                <p>Lorem, ipsum.</p>
+              </div>
+              <div className="available-ticket-card-tours">
+                <p>Available Today</p>
+                <div className='ticket-available-ticket-card-tours'>
+                  <img src={TicketIcon} className="icon-available-ticket-card-tours" alt="" srcset="" />
+                  <p>300 Sold</p>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div className="container-card-tours">
+            <div className="body-card-tours">
+              <Slider {...settings}>
+                <img src="https://images.unsplash.com/photo-1470770841072-f978cf4d019e?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1470&q=80" alt="" srcset="" className='img-card-tour' />
+                <img src="https://images.unsplash.com/photo-1501785888041-af3ef285b470?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1470&q=80" alt="" srcset="" className='img-card-tour' />
+                <img src="https://images.unsplash.com/photo-1470770841072-f978cf4d019e?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1470&q=80" alt="" srcset="" className='img-card-tour' />
+              </Slider>
+              <div className="content-top-card">
+                <button className='button-top-card'>lorem</button>
+                <span className="lnr lnr-heart" style={{ color: 'white', fontSize: 20, fontWeight: 'bold' }}></span>
+              </div>
+              <div className="content-bottom-card">
+                <span className="lnr lnr-map-marker text-content-bottom-card"></span>
+                <p>Bandung</p>
+              </div>
+            </div>
+            <div className="content-card-tours">
+              <div className="review-content-card-tours">
+                <ReactStars
+                  count={5}
+                  onChange={null}
+                  size={24}
+                  value={4}
+                  activeColor="#FF4248"
+                />
+                <p className='text-review-content-card-tours'>24 Reviews</p>
+              </div>
+              <p className="title-card-tours">
+                Lorem ipsum dolor sit amet consectetur.
+              </p>
+              <div className="description-card-tours">
+                <p>Lorem ipsum dolor sit.</p>
+                <p>Lorem, ipsum.</p>
+              </div>
+              <div className="description-card-tours">
+                <p>Lorem, ipsum.</p>
+                <p>Lorem, ipsum.</p>
+              </div>
+              <div className="description-card-tours">
+                <p>Lorem, ipsum.</p>
+                <p>Lorem, ipsum.</p>
+              </div>
+              <div className="available-ticket-card-tours">
+                <p>Available Today</p>
+                <div className='ticket-available-ticket-card-tours'>
+                  <img src={TicketIcon} className="icon-available-ticket-card-tours" alt="" srcset="" />
+                  <p>300 Sold</p>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div className="container-card-tours">
+            <div className="body-card-tours">
+              <Slider {...settings}>
+                <img src="https://images.unsplash.com/photo-1470770841072-f978cf4d019e?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1470&q=80" alt="" srcset="" className='img-card-tour' />
+                <img src="https://images.unsplash.com/photo-1501785888041-af3ef285b470?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1470&q=80" alt="" srcset="" className='img-card-tour' />
+                <img src="https://images.unsplash.com/photo-1470770841072-f978cf4d019e?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1470&q=80" alt="" srcset="" className='img-card-tour' />
+              </Slider>
+              <div className="content-top-card">
+                <button className='button-top-card'>lorem</button>
+                <span className="lnr lnr-heart" style={{ color: 'white', fontSize: 20, fontWeight: 'bold' }}></span>
+              </div>
+              <div className="content-bottom-card">
+                <span className="lnr lnr-map-marker text-content-bottom-card"></span>
+                <p>Bandung</p>
+              </div>
+            </div>
+            <div className="content-card-tours">
+              <div className="review-content-card-tours">
+                <ReactStars
+                  count={5}
+                  onChange={null}
+                  size={24}
+                  value={4}
+                  activeColor="#FF4248"
+                />
+                <p className='text-review-content-card-tours'>24 Reviews</p>
+              </div>
+              <p className="title-card-tours">
+                Lorem ipsum dolor sit amet consectetur.
+              </p>
+              <div className="description-card-tours">
+                <p>Lorem ipsum dolor sit.</p>
+                <p>Lorem, ipsum.</p>
+              </div>
+              <div className="description-card-tours">
+                <p>Lorem, ipsum.</p>
+                <p>Lorem, ipsum.</p>
+              </div>
+              <div className="description-card-tours">
+                <p>Lorem, ipsum.</p>
+                <p>Lorem, ipsum.</p>
+              </div>
+              <div className="available-ticket-card-tours">
+                <p>Available Today</p>
+                <div className='ticket-available-ticket-card-tours'>
+                  <img src={TicketIcon} className="icon-available-ticket-card-tours" alt="" srcset="" />
+                  <p>300 Sold</p>
+                </div>
+              </div>
             </div>
           </div>
         </div>
